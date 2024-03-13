b13b2daaaa45bbe94af2aec49371a63e20e34132
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-03-14 00:44:40 +0530

    string question 2

--- string-question2.py
+++ string-question2.py
@@ -1,4 +1,7 @@
 string="abfk"
-for i in range(0,len(string)):
-    dif=str(ord(string[i])-ord(string[i-1]))
-    print(string[i-1]+dif,end="")
+for i in range(1,len(string)+1):
+    if i<len(string):
+        dif=str(ord(string[i])-ord(string[i-1]))
+        print(string[i-1]+dif,end="")
+    else:
+        print(string[i-1],end="")
