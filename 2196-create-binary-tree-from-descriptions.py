098725369dc66b751353517767406e122e2d6ca0
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-07-15 15:26:39 +0530

    2196

--- 2196-create-binary-tree-from-descriptions.py
+++ 2196-create-binary-tree-from-descriptions.py
@@ -22,3 +22,32 @@ Explanation: The root node is the node with value 1 since it has no parent.
 The resulting binary tree is shown in the diagram.
 
 """
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def createBinaryTree(self, descriptions: list[list[int]]) -> TreeNode:
+        contains = {}
+        children = set()
+        for par, child, isleft in descriptions:
+            children.add(child)
+            if par not in contains:
+                contains[par] = TreeNode(par)
+            if child not in contains:
+                contains[child] = TreeNode(child)
+            if isleft:
+                contains[par].left = contains[child]
+            else:
+                contains[par].right = contains[child]
+
+        for p, c, l in descriptions:
+            if p not in children:
+                return contains[p]
+
+if __name__ == "__main__":
+    descriptions = [[20,15,1],[20,17,0],[50,20,1],[50,80,0],[80,19,1]]
+    sol = Solution()
+    print(f"The root node is : {sol.createBinaryTree(descriptions)}")
