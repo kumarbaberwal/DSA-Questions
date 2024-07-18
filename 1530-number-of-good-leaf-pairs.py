5682614c0ba0cb42fa8f0e4ef999fe80911e9882
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-07-18 23:13:51 +0530

    1530

---
+++ 1530-number-of-good-leaf-pairs.py
@@ -0,0 +1,22 @@
+"""
+You are given the root of a binary tree and an integer distance. A pair of two different leaf nodes of a binary tree is said to be good if the length of the shortest path between them is less than or equal to distance.
+
+Return the number of good leaf node pairs in the tree.
+
+Input: root = [1,2,3,null,4], distance = 3
+Output: 1
+Explanation: The leaf nodes of the tree are 3 and 4 and the length of the shortest path between them is 3. This is the only good pair.
+
+Input: root = [1,2,3,4,5,6,7], distance = 3
+Output: 2
+Explanation: The good pairs are [4,5] and [6,7] with shortest path = 2. The pair [4,6] is not good because the length of ther shortest path between them is 4.
+"""
+
+from collections import defaultdict
+
+
+if __name__ == "__main__":
+    count = defaultdict(int)
+    count[1] = 1
+    print(count)
+    print(type(count))
